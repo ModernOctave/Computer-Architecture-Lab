4af505a41a391e135e817ff63b014354664397aa
Om Patil <200010036@iitdh.ac.in>
2022-08-08 21:37:20 +0530

    Added expectedTime function

--- assignment0/Main.java
+++ assignment0/Main.java
@@ -1,7 +1,16 @@
 public class Main {
     
     public static void main(String[] args){
+        int[] widths = {6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+        double[] ps = {0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.65, 0.7, 0.75, 0.8};
+
         Simulation sim = new Simulation();
-        sim.run(6, 0.1);
+        for (int width : widths) {
+            for (double p : ps) {
+                double expTime = sim.getExpectedTime(width, p, 0.001, 100);
+                System.out.print(expTime + ", ");
+            }
+            System.out.println();
+        }
     }
 }
--- assignment0/Simulation.java
+++ assignment0/Simulation.java
@@ -1,39 +1,71 @@
 public class Simulation {
+    Boolean border_crossed, intruder_detected;
+    Clock clock;
+    Border border;
+    Infiltrator infil;
 
     public Simulation() {
     }
 
+    public void run(int width, double p, boolean debug) {
+        run(width, p);
+
+        if (debug) {
+            if(border.border_crossed)
+            {
+                System.out.println("Intruder crossed successfully!");
+                System.out.println("p: "+ p);
+                System.out.println("width: " + width);
+                System.out.println("Time taken: " + clock.time);
+            }
+
+            if(border.intruder_detected)
+            {
+                System.out.println("Intruder detected!");
+                System.out.println("p: "+ p);
+                System.out.println("width: " + width);
+                System.out.println("Time taken: " + clock.time);
+            }
+        }
+    }
+
     public void run(int width, double p) {
-        Border border = new Border(width, p);
+        border = new Border(width, p);
 
-        Clock uniClock = new Clock();
+        clock = new Clock();
 
-        Infiltrator infil = new Infiltrator();
+        infil = new Infiltrator();
 
         while( !border.border_crossed && !border.intruder_detected)
         {
             border.runDutyCycle();
             border.updateView();
             Integer[] Position = infil.NextMove(border.neighbours_status, border.On_sensors);
-            uniClock.incrimentTime(1); 
+            clock.incrimentTime(1); 
             border.moveIntruder(Position[0], Position[1]);
-            uniClock.incrimentTime(9);   
+            clock.incrimentTime(9);   
         }
+    }
 
-        if(border.border_crossed)
-        {
-            System.out.println("Intruder crossed successfully!");
-            System.out.println("p: "+ p);
-            System.out.println("width: " + width);
-            System.out.println("Time taken: " + uniClock.time);
-        }
+    public double getExpectedTime(int width, double p, double epsilon, int iterations) {
+        int n = 0, sum = 0, times = 0;
+        double expectedTime = 0, prevExpectedTime = 0;
+        do {
+            n++;
+            run(width, p);
+            sum += clock.time;
+            expectedTime = (double)sum / n;
+            // System.out.println(expectedTime);
+            if (Math.abs(expectedTime - prevExpectedTime) < epsilon) {
+                times++;
+            }
+            else {
+                times = 0;
+                prevExpectedTime = expectedTime;
+            }
+        } while (times < iterations);
+            
 
-        if(border.intruder_detected)
-        {
-            System.out.println("Intruder detected!");
-            System.out.println("p: "+ p);
-            System.out.println("width: " + width);
-            System.out.println("Time taken: " + uniClock.time);
-        }
+        return expectedTime;
     }
 }
