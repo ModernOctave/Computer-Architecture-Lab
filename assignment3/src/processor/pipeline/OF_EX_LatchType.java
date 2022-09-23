56f8416f55a03e4a0aca45b4b54ea275491d0197
hrishikesh2002 <200010037@iitdh.ac.in>
2022-09-23 16:07:31 +0530

    OF_EX latch done

--- assignment3/src/processor/pipeline/OF_EX_LatchType.java
+++ assignment3/src/processor/pipeline/OF_EX_LatchType.java
@@ -3,6 +3,13 @@ package processor.pipeline;
 public class OF_EX_LatchType {
 	
 	boolean EX_enable;
+	int opcode;
+	int imm;
+	int aluResult;
+	int result;
+	int op1;
+	int op2;
+
 	
 	public OF_EX_LatchType()
 	{
@@ -15,6 +22,56 @@ public class OF_EX_LatchType {
 
 	public void setEX_enable(boolean eX_enable) {
 		EX_enable = eX_enable;
+	}
+
+	public int getOpcode() {
+		return opcode;
+	}
+
+	public int getImm() {
+		return imm;
+	}
+
+	public int getOp1() {
+		return op1;
+	}
+
+	public int getOp2() {
+		return op2;
+	}
+
+	public int getAluResult() {
+		return aluResult;
+	}
+
+	public int getResult() {
+		return result;
+	}
+
+
+	public void setOpcode(int opcode) {
+		this.opcode = opcode;
+	}
+	
+	public void setImm(int imm) {
+		this.imm = imm;
+	}
+
+	public void setOp1(int op1) {
+		this.op1 = op1;
+	}
+
+	public void setOp2(int op2) {
+		this.op2 = op2;
+	}
+
+
+	public void setAluResult(int aluResult) {
+		this.aluResult = aluResult;
+	}
+
+	public void setResult(int result) {
+		this.result = result;
 	}
 
 }
