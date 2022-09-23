e7d89427a36869e4eee3f739abccb47b6a6cc2a7
Om Patil <200010036@iitdh.ac.in>
2022-09-23 14:57:39 +0530

    Begin Assignment 3

---
+++ assignment3/src/generic/Simulator.java
@@ -0,0 +1,78 @@
+package generic;
+
+import java.io.FileInputStream;
+import java.math.BigInteger;
+
+import processor.Clock;
+import processor.Processor;
+
+public class Simulator {
+		
+	static Processor processor;
+	static boolean simulationComplete;
+	
+	public static void setupSimulation(String assemblyProgramFile, Processor p)
+	{
+		Simulator.processor = p;
+		loadProgram(assemblyProgramFile);
+		
+		simulationComplete = false;
+	}
+	
+	static void loadProgram(String assemblyProgramFile)
+	{
+		/*
+		 * TODO
+		 * 1. load the program into memory according to the program layout described
+		 *    in the ISA specification
+		 * 2. set PC to the address of the first instruction in the main
+		 * 3. set the following registers:
+		 *     x0 = 0
+		 *     x1 = 65535
+		 *     x2 = 65535
+		 */
+		try {
+			FileInputStream file = new FileInputStream(assemblyProgramFile);
+			byte[] b = new byte[4];
+			file.read(b);
+			int pc = new BigInteger(b).intValue();
+			processor.getRegisterFile().setProgramCounter(pc);
+			processor.getRegisterFile().setValue(0, 0);
+			processor.getRegisterFile().setValue(1, 65535);
+			processor.getRegisterFile().setValue(2, 65535);
+			int i = 0;
+			while(file.read(b) != -1) {
+				processor.getMainMemory().setWord(i, new BigInteger(b).intValue());
+				i++;
+			}
+			file.close();
+		} catch (Exception e) {
+			Misc.printErrorAndExit("[Load Program Error]: " + e.getMessage());
+		}
+	}
+	
+	public static void simulate()
+	{
+		while(simulationComplete == false)
+		{
+			processor.getIFUnit().performIF();
+			Clock.incrementClock();
+			processor.getOFUnit().performOF();
+			Clock.incrementClock();
+			processor.getEXUnit().performEX();
+			Clock.incrementClock();
+			processor.getMAUnit().performMA();
+			Clock.incrementClock();
+			processor.getRWUnit().performRW();
+			Clock.incrementClock();
+		}
+		
+		// TODO
+		// set statistics
+	}
+	
+	public static void setSimulationComplete(boolean value)
+	{
+		simulationComplete = value;
+	}
+}
