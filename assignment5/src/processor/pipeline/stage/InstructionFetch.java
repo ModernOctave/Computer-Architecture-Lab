6c15ab524f16461ed8da865d1be23ce557e5eb3b
Om Patil <200010036@iitdh.ac.in>
2022-10-19 18:13:05 +0530

    Assignment 5 Init

---
+++ assignment5/src/processor/pipeline/stage/InstructionFetch.java
@@ -0,0 +1,89 @@
+package processor.pipeline.stage;
+
+import generic.Simulator;
+import generic.Statistics;
+import processor.Processor;
+import processor.pipeline.latch.EX_IF_LatchType;
+import processor.pipeline.latch.IF_EnableLatchType;
+import processor.pipeline.latch.IF_OF_LatchType;
+
+public class InstructionFetch {
+	
+	Processor containingProcessor;
+	IF_EnableLatchType IF_EnableLatch;
+	IF_OF_LatchType IF_OF_Latch;
+	EX_IF_LatchType EX_IF_Latch;
+	
+	public InstructionFetch(Processor containingProcessor, IF_EnableLatchType iF_EnableLatch, IF_OF_LatchType iF_OF_Latch, EX_IF_LatchType eX_IF_Latch)
+	{
+		this.containingProcessor = containingProcessor;
+		this.IF_EnableLatch = iF_EnableLatch;
+		this.IF_OF_Latch = iF_OF_Latch;
+		this.EX_IF_Latch = eX_IF_Latch;
+	}
+	
+	public void performIF()
+	{
+		
+		if(IF_EnableLatch.isIF_enable())
+		{
+			
+			if(!IF_EnableLatch.isBubbled())
+			{
+				// Update PC
+				if(!IF_EnableLatch.isStalled())
+				{
+					if(containingProcessor.getIsBranchTaken())
+					{
+						containingProcessor.getRegisterFile().setProgramCounter(containingProcessor.getBranchPC());
+						containingProcessor.setIsBranchTaken(false);
+						if(Simulator.isDebugMode())
+						{
+							System.out.println("[Debug] (IF) Branch taken, PC updated to " + containingProcessor.getBranchPC());
+						}
+					}
+
+					if(Simulator.isDebugMode())
+					{
+						System.out.println("[Debug] (IF) Instruction fetched from " + containingProcessor.getRegisterFile().getProgramCounter());
+					}
+
+					// IF stage
+					int PC = containingProcessor.getRegisterFile().getProgramCounter();
+					IF_OF_Latch.setPc(PC);
+					int instruction = containingProcessor.getMainMemory().getWord(PC);
+					IF_OF_Latch.setInstruction(instruction);
+
+					Statistics.setNumberOfDynamicInstructions(Statistics.getNumberOfDynamicInstructions() + 1);
+
+					containingProcessor.getRegisterFile().setProgramCounter(containingProcessor.getRegisterFile().getProgramCounter() + 1);
+					if(Simulator.isDebugMode())
+					{
+						System.out.println("[Debug] (IF) PC incremented to " + containingProcessor.getRegisterFile().getProgramCounter());
+					}
+				}
+				else
+				{
+					IF_EnableLatch.setIsStalled(false);
+					if(Simulator.isDebugMode())
+					{
+						System.out.println("[Debug] (IF) Stalled instruction fetched from " + containingProcessor.getRegisterFile().getProgramCounter());
+					}
+				}
+			}
+				
+			// Set OF_enable
+			IF_OF_Latch.setOF_enable(true);
+
+			// Pass the bubble signal to the next stage
+			IF_OF_Latch.setIsBubbled(IF_EnableLatch.isBubbled());
+		}
+
+
+		if(Simulator.isDebugMode()) 
+		{
+			System.out.println();
+		}
+	}
+
+}
---
+++ assignment5/todo.md
@@ -0,0 +1,4 @@
+## Optimize
+- [ ] Handle end instruction better
+- [ ] Control interlock only if branchPC is not pc+1
+
